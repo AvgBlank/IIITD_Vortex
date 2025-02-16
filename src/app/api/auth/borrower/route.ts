e48c65f4e8a419dc0a0762b1e36ee547ac77c41a
Husain Khorakiwala <hussainkhorakiwala45@gmail.com>
2025-02-16 09:55:20 +0530

    Fixing another deployement error

--- src/app/api/auth/borrower/route.ts
+++ src/app/api/auth/borrower/route.ts
@@ -13,7 +13,7 @@ export async function GET(request: NextRequest) {
     const body = await request.json();
     const { email, password } = body;
 
-    const user = await prisma.BorrowerUser.findUnique({
+    const user = await prisma.borrowerUser.findUnique({
       where: { email },
     });
 
@@ -78,7 +78,7 @@ export async function POST(request: Request) {
     }
 
     // Check if email doesn't already exist
-    const checkEmail = await prisma.BorrowerUser.findUnique({ where: { email } });
+    const checkEmail = await prisma.borrowerUser.findUnique({ where: { email } });
     if (checkEmail) {
       return NextResponse.json(
         { error: "User with this email already exists" },
@@ -93,12 +93,12 @@ export async function POST(request: Request) {
     let randomId = Math.floor(1000000000 + Math.random() * 9000000000);
 
     while (
-      await prisma.BorrowerUser.findUnique({ where: { id: randomId.toString() } })
+      await prisma.borrowerUser.findUnique({ where: { id: randomId.toString() } })
     ) {
       randomId = Math.floor(1000000000 + Math.random() * 9000000000);
     }
 
-    const newUser = await prisma.BorrowerUser.create({
+    const newUser = await prisma.borrowerUser.create({
       data: {
         id: randomId.toString(),
         name,
