d95e35a169ea0d7bba3f39789fe0dad408d724df
Husain Khorakiwala <hussainkhorakiwala45@gmail.com>
2025-02-16 09:46:12 +0530

    Deployement error

--- src/app/api/auth/lender/route.ts
+++ src/app/api/auth/lender/route.ts
@@ -13,7 +13,7 @@ export async function GET(request: NextRequest) {
     const body = await request.json();
     const { email, password } = body;
 
-    const user = await prisma.LenderUser.findUnique({
+    const user = await prisma.lenderUser.findUnique({
       where: { email },
     });
 
@@ -78,7 +78,7 @@ export async function POST(request: Request) {
     }
 
     // Check if email doesn't already exist
-    const checkEmail = await prisma.LenderUser.findUnique({ where: { email } });
+    const checkEmail = await prisma.lenderUser.findUnique({ where: { email } });
     if (checkEmail) {
       return NextResponse.json(
         { error: "User with this email already exists" },
@@ -93,12 +93,12 @@ export async function POST(request: Request) {
     let randomId = Math.floor(1000000000 + Math.random() * 9000000000);
 
     while (
-      await prisma.LenderUser.findUnique({ where: { id: randomId.toString() } })
+      await prisma.lenderUser.findUnique({ where: { id: randomId.toString() } })
     ) {
       randomId = Math.floor(1000000000 + Math.random() * 9000000000);
     }
 
-    const newUser = await prisma.LenderUser.create({
+    const newUser = await prisma.lenderUser.create({
       data: {
         id: randomId.toString(),
         name,
